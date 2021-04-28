e21f3e77b59b2989ad441e05509c51abdf1dce25
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-04-28 19:30:24 +0100

    Commiting missing files

--- src/apis/giphy.js
+++ src/apis/giphy.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const KEY = 'HRqYLT8rSs8kJbbogj02Z0mbrWnVVrGE';
 
 export default axios.create({
-    baseURL: 'https://api.giphy.com/v1/gifs/',
+    baseURL: 'https://api.giphy.com/v1/gifs/search?api_key=',
     params: {
         maxResults: 30,
         key: KEY
