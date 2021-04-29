942263fdd2ea7f154272b276e635d98a90bf2ed4
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-04-29 07:44:28 +0100

    Adding project to repo

---
+++ build/precache-manifest.3d59e3e627254fac3f1228c93139f266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be08ba7eaf0823a821e",
+    "url": "/giphy/static/js/runtime~main.f83d3e2c.js"
+  },
+  {
+    "revision": "a133e61ea06fd48dac83",
+    "url": "/giphy/static/js/main.b85a6c0c.chunk.js"
+  },
+  {
+    "revision": "e063fd5ec701a276836b",
+    "url": "/giphy/static/js/2.87f9b28f.chunk.js"
+  },
+  {
+    "revision": "a133e61ea06fd48dac83",
+    "url": "/giphy/static/css/main.d218ab5d.chunk.css"
+  },
+  {
+    "revision": "34bfc6c63fbdf2113da9fe073e49c6bd",
+    "url": "/giphy/index.html"
+  }
+];
