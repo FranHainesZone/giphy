19e44fed2188fa8ad6163bc935ad2cd780c434fc
FranHainesZone <52914701+FranHainesZone@users.noreply.github.com>
2021-04-29 07:40:32 +0100

    Updates

---
+++ precache-manifest.46d52f15137dfcdd49e7fa79e4b69156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be08ba7eaf0823a821e",
+    "url": "/giphy/static/js/runtime~main.f83d3e2c.js"
+  },
+  {
+    "revision": "a133e61ea06fd48dac83",
+    "url": "/giphy/static/js/main.b85a6c0c.chunk.js"
+  },
+  {
+    "revision": "e063fd5ec701a276836b",
+    "url": "/giphy/static/js/2.87f9b28f.chunk.js"
+  },
+  {
+    "revision": "a133e61ea06fd48dac83",
+    "url": "/giphy/static/css/main.d218ab5d.chunk.css"
+  },
+  {
+    "revision": "08ef315002cd4bc91223d0bd5ff81623",
+    "url": "/giphy/index.html"
+  }
+];
